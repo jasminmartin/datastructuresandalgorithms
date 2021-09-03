cfa94e43f940862c7f4996a2ef2ac0651897de3a
Jaz Martin <jasmin.martin@sky.uk>
2021-09-03 10:00:55 +0100

    Binary search

--- week2/Main.cpp
+++ week2/Main.cpp
@@ -13,7 +13,9 @@ void Swap(int *a, int *b) {
 
 void SelectionSort(int *a, int size) {
   int smallest;
+  //{5,1,3,4,2}
   for (int n = 0; n < size - 1; n++) {
+    // locate the smallest unprocessed element
     smallest = n;
     for (int i = n + 1; i < size - 1; i++) {
       if (a[i] < a[smallest]) {
@@ -24,6 +26,8 @@ void SelectionSort(int *a, int size) {
   }
 }
 
+// Compare pairs of adjacent elements of the sequence, if they are
+// in the wrong order swap them and do this till there are no more
 void BubbleSort(int *a, int size) {
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size - 1; j++) {
@@ -35,24 +39,47 @@ void BubbleSort(int *a, int size) {
 }
 
 void ModifiedBubbleSort(int *a, int size) {
-    bool flag = false;
+  bool flag = false;
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size - 1; j++) {
       if (a[j] > a[j + 1]) {
         Swap(&a[j], &a[j + 1]);
         flag = true;
       }
-      if (flag==true) {
-          break;
+      if (flag == true) {
+        break;
       }
     }
   }
 }
 
+// Each element is checked in sequence until a match is found
 int SequentialSearch(int *a, int size, int key) {
   for (int i = 0; i < size - 1; i++) {
     if (a[i] == key) {
       return 1;
+    }
+  }
+}
+
+// Searches large sorted data. The key is compared to the middle value, if not
+// matched and the key is less than the value the action is repeated on the left
+// portion of the array otherwise the right.
+int BinarySearch(int a[], int size, int key) {
+  int middle = 0;
+  int lower = 0;
+  int upper = size - 1;
+  for (int i = 0; i < size - 1; i++) {
+    middle = (lower + upper) / 2;
+    if (key < a[middle]) {
+      upper = middle - 1;
+    } else if (key > a[middle]) {
+      lower = middle + 1;
+    } else {
+      return middle;
+    }
+    if (lower > upper) {
+      return -1;
     }
   }
 }
@@ -62,6 +89,7 @@ void Print(int *a, int size) {
     cout << a[i] << " ";
   }
 }
+
 int main() {
   cout << "Exercise 1 - Selection Sort" << endl;
   cout << "Before Sort" << endl;
@@ -100,14 +128,14 @@ int main() {
   cout << "Exercise 4 - BubbleSort" << endl;
   cout << "Before Sort" << endl;
   int data2[] = {4, 5, 6, 1, 3, 9, 4, 8, 2, 7};
-//   Print(data2, 10);
-//   cout << "After Sort" << endl;
+  //   Print(data2, 10);
+  //   cout << "After Sort" << endl;
   auto t3 = high_resolution_clock::now();
   BubbleSort(bigRandArray, bigRandSize);
   auto t4 = high_resolution_clock::now();
   duration<double, std::milli> ms_double_bubble = t4 - t3;
   std::cout << "BubbleSort Time: " << ms_double_bubble.count() << "ms";
-//   Print(data2, 10);
+  //   Print(data2, 10);
 
   cout << "Exercise 5" << endl;
   cout << "Modified Bubble Sort" << endl;
@@ -115,15 +143,35 @@ int main() {
   ModifiedBubbleSort(bigRandArray, bigRandSize);
   auto t6 = high_resolution_clock::now();
   duration<double, std::milli> ms_modified_double_bubble = t6 - t5;
-  std::cout << "Modified BubbleSort Time: " << ms_modified_double_bubble.count() << "ms";
-
+  std::cout << "Modified BubbleSort Time: " << ms_modified_double_bubble.count()
+            << "ms";
 
   cout << "Exercise 6" << endl;
   cout << "Sequential Search" << endl;
   int data3[] = {4, 5, 6, 1, 3, 9, 4, 8, 2, 7};
   Print(data3, 10);
+  auto t7 = high_resolution_clock::now();
   int found = SequentialSearch(data3, 10, 3);
+  auto t8 = high_resolution_clock::now();
+  duration<double, std::milli> ms_sequential_search = t8 - t7;
+  std::cout << "Sequential Search Time: " << ms_sequential_search.count()
+            << "ms";
   if (found != -1) {
+    cout << "Key exists in the array " << endl;
+  } else {
+    cout << "Key does not exist in the array " << endl;
+  }
+
+  cout << "Exercise 7" << endl;
+  cout << "Binary Search" << endl;
+  int data4[] = {4, 5, 6, 1, 3, 9, 4, 8, 2, 7};
+  auto t9 = high_resolution_clock::now();
+  int foundBinary = BinarySearch(data4, 10, 3);
+  auto t10 = high_resolution_clock::now();
+  duration<double, std::milli> ms_binary_search = t10 - t9;
+  std::cout << "Binary Search Time: " << ms_binary_search.count()
+            << "ms" <<endl;
+  if (foundBinary != -1) {
     cout << "Key exists in the array " << endl;
   } else {
     cout << "Key does not exist in the array " << endl;
